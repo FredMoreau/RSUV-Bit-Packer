fc3f3e1bc2ee0d5732611d941bf57a77713f3147
Fred Moreau <frederic.moreau@unity3d.com>
2026-03-23 21:27:34 +0100

    Fixed Int4 sample property

--- Samples~/More Renderer Properties/RendererInt4Property.cs
+++ Samples~/More Renderer Properties/RendererInt4Property.cs
@@ -2,17 +2,17 @@ using System;
 
 namespace UnityEngine.RSUVBitPacker
 {
-    [Serializable, RendererValueTypeName("Int4"), RendererValueTypeLength(8)]
+    [Serializable, RendererValueTypeName("Int4"), RendererValueTypeLength(4)]
     public class RendererInt4Property : RendererProperty<int>
     {
-        [SerializeField, Range(0, 31)]
+        [SerializeField, Range(0, 15)]
         private int _value = default;
 
         public override int Value { get => _value; set => _value = value; }
 
         public override uint Length => 4;
 
-        public override uint Data => (uint)Mathf.Clamp(_value, 0, 31);
+        public override uint Data => (uint)Mathf.Clamp(_value, 0, 15);
 
         public override string hlslType => "int";
 
